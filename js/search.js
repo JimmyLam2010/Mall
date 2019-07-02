0d6bb9edc689c9cc041d0c08598b74b8dee3036c
JimmyLam2010 <jimmy201028@gmail.com>
2019-07-02 11:14:36 +0800

    case close

---
+++ js/search.js
@@ -0,0 +1,8 @@
+var search = document.getElementById("search");
+console.log(search);
+var scan = document.getElementById("scan");
+console.log(scan);
+
+search.onclick = function(){
+    window.location.href="search.html";
+}
