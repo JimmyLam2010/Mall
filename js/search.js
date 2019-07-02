2fb6e3d54d2c520a4b8d179fad9b99c769ad40bb
JimmyLam2010 <jimmy201028@gmail.com>
2019-07-02 13:48:28 +0800

    case really close

--- js/search.js
+++ js/search.js
@@ -1,8 +1,10 @@
 var search = document.getElementById("search");
-console.log(search);
-var scan = document.getElementById("scan");
-console.log(scan);
 
 search.onclick = function(){
     window.location.href="search.html";
 }
+
+function backTo(){
+    window.history.back();
+}
+
